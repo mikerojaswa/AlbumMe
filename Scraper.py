c7350858112dd398f88d96785861d489ed52534d
Michael Rojas <mikerojaswa@gmail.com>
2017-05-11 04:53:28 -0700

    CSS added

--- Scraper.py
+++ Scraper.py
@@ -4,6 +4,7 @@ from lxml import html
 from bs4 import BeautifulSoup
 import requests
 import json
+import re
 
 songObjects = []
 
@@ -15,8 +16,18 @@ page2 = requests.get('http://www.hotnewhiphop.com/top100/')
 contents2 = page2.content
 soup2 = BeautifulSoup(contents2, 'html.parser')
 
+page3 = requests.get('https://www.beatport.com/top-100')
+contents3 = page3.content
+soup3 = BeautifulSoup(contents3, 'html.parser')
+
 hypemTrackInfo = soup.find_all(class_='track_name')
 hiphopTrackInfo = soup2.find_all(class_='chartItem-body')
+
+#S/O to: http://stackoverflow.com/questions/13323976/how-to-extract-a-json-object-that-was-defined-in-a-html-page-javascript-block-us
+script = soup3.find(id="data-objects", text=re.compile('window\.Playables'))
+json_text = re.search(r'^\s*window\.Playables\s*=\s*({.*?})\s*;\s*$', script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+beatportData = json.loads(json_text)
+
 
 hypeData = {
     'songs': []
@@ -29,11 +40,12 @@ hiphopData = {
 for item in hiphopTrackInfo:
     trackName = item.find(class_='cover-title chartItem-artist-trackTitle').string.strip()
     artistName = item.find(class_='chartItem-artist-artistName').string
-    print(trackName + " - " + artistName)
     hiphopData['songs'].append({
         'track': trackName,
         'artist': artistName
     })
+
+
 
 
 for item in hypemTrackInfo:
@@ -45,12 +57,15 @@ for item in hypemTrackInfo:
         'track': songName,
         'artist': artist
     })
-        
+
 with open("hypem.json", "w") as json_file:
     json.dump(hypeData, json_file, indent = 5)
 
 with open("hiphop.json", "w") as json_file:
     json.dump(hiphopData, json_file, indent = 5)
+
+with open("beatport.json", "w") as json_file:
+    json.dump(beatportData, json_file, indent = 5)
 
 with open('hypem.json') as data_file:    
     data = json.load(data_file)  
--- script.js
+++ script.js
@@ -1,97 +1,75 @@
-var elem = document.getElementById("square");
 var songArray = [];
-var counter = 0;
-
+var globalCounter = 0;
 var hypeJson;
-$.getJSON("http://www.michaelarojas.com/AlbumMe/hypem.json", function(json) {
-  hypeJson = json;
+$.getJSON("http://www.michaelarojas.com/AlbumMe/hypem.json", function (json) {
+    hypeJson = json;
 });
-
 var rapJson;
-$.getJSON("http://www.michaelarojas.com/AlbumMe/hiphop.json", function(json) {
-  rapJson = json;
+$.getJSON("http://www.michaelarojas.com/AlbumMe/hiphop.json", function (json) {
+    rapJson = json;
 });
-
 function randomItunes() {
     var randomNumberBetween0and19 = Math.floor(Math.random() * 99);
-   
     var title = "Title : " + songArray[randomNumberBetween0and19].title;
     var artist = "Artist : " + songArray[randomNumberBetween0and19].artist;
     var category = "Genre : " + songArray[randomNumberBetween0and19].genre;
     var imagesrc = songArray[randomNumberBetween0and19].image;
-    document.getElementById("h2").innerHTML =  title;
-    document.getElementById("h21").innerHTML =  artist;
-    document.getElementById("h22").innerHTML =  category;
-    document.getElementById("imageid").src=imagesrc;
+    document.getElementById("h2").innerHTML = title;
+    document.getElementById("h21").innerHTML = artist;
+    document.getElementById("h22").innerHTML = category;
+    document.getElementById("imageid").src = imagesrc;
 }
-
-function randomHype(){
-    if(counter != 20){
-        counter = counter + 1;
-    }else{
-        counter = 0;
+function randomHype() {
+    if (globalCounter != 20) {
+        globalCounter = globalCounter + 1;
     }
-    document.getElementById("h2").innerHTML =  hypeJson.songs[counter].track;
-    document.getElementById("h21").innerHTML =  hypeJson.songs[counter].artist;
-    document.getElementById("h22").innerHTML =  "";
-    document.getElementById("imageid").src="hypem.png";
-}
-
-function randomRap(){
-    if(counter != 20){
-        counter = counter + 1;
-    }else{
-        counter = 0;
+    else {
+        globalCounter = 0;
     }
-    document.getElementById("h2").innerHTML =  rapJson.songs[counter].track;
-    document.getElementById("h21").innerHTML =  rapJson.songs[counter].artist;
-    document.getElementById("h22").innerHTML =  "";
-    document.getElementById("imageid").src="hothiphop.png";
+    document.getElementById("h2").innerHTML = hypeJson.songs[globalCounter].track;
+    document.getElementById("h21").innerHTML = hypeJson.songs[globalCounter].artist;
+    document.getElementById("h22").innerHTML = "";
+    document.getElementById("imageid").src = "hypem.png";
 }
-
-function musicSrcDelegate(){
-   var src = document.getElementById("musicSource");
+function randomRap() {
+    if (globalCounter != 20) {
+        globalCounter = globalCounter + 1;
+    }
+    else {
+        globalCounter = 0;
+    }
+    document.getElementById("h2").innerHTML = rapJson.songs[globalCounter].track;
+    document.getElementById("h21").innerHTML = rapJson.songs[globalCounter].artist;
+    document.getElementById("h22").innerHTML = "";
+    document.getElementById("imageid").src = "hothiphop.png";
+}
+function musicSrcDelegate() {
+    var src = document.getElementById("musicSource");
     var srcVal = src.value;
-    if(srcVal == "itunes"){
+    if (srcVal == "itunes") {
         randomItunes();
     }
-    if(srcVal == "hypemachine"){
+    if (srcVal == "hypemachine") {
         randomHype();
     }
-    if(srcVal == "rap"){
+    if (srcVal == "rap") {
         randomRap();
     }
     console.log(srcVal);
 }
 var url = "https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf=143441/limit=100/genre=7/explicit=true/rss.xml";
-    
 $.get(url, function (data) {
-$(data).find("item").each(function () { 
+    $(data).find("item").each(function () {
         var el = $(this);
         var input = el.find("coverArt").text();
         var fields = input.split('http');
         var imageInput = "http" + fields[3];
-   
-        var Song = {"title": el.find("title").text(), 
-                    "genre": el.find("category").text(),
-                    "description": el.find("description").text(), 
-                    "artist": el.find("artist").text(),
-                    "image": imageInput
-                   };
+        var Song = { "title": el.find("title").text(),
+            "genre": el.find("category").text(),
+            "description": el.find("description").text(),
+            "artist": el.find("artist").text(),
+            "image": imageInput
+        };
         songArray.push(Song);
-//        console.log("------------------------");
-//        console.log("title      : " + el.find("title").text());
-//        console.log("Genre     : " + el.find("category").text());
-//        console.log("description: " + el.find("description").text());
-//        console.log("artist: " + el.find("artist").text());
-    if(songArray[10] != undefined){
-        console.log(songArray[10].image);
-    }
-    
     });
 });
-
-
-
-
-
